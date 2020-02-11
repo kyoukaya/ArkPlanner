6739f5a3ca87771d64a29fc469d5a99af7d6f336
kyoukaya <kyoukaya@gmail.com>
2020-02-11 21:20:34 +0800

    async updates for penguinstats

--- server.py
+++ server.py
@@ -1,18 +1,17 @@
+import asyncio
+from signal import SIGINT, signal
+
 from sanic import Sanic, response
+
 from MaterialPlanning import MaterialPlanning
-import time, codecs
 
 app = Sanic(name="ArkPlanner")
-
-
 mp = MaterialPlanning()
 mp.update()
-last_updated = time.time()
 
 
 @app.route("/plan", methods=["POST"])
 async def plan(request):
-    global last_updated
     try:
         input_data = request.json
         owned_dct = input_data["owned"]
@@ -36,9 +35,6 @@ async def plan(request):
         gold_demand = True
 
     try:
-        if time.time() - last_updated > 60 * 30:
-            mp.update()
-            last_updated = time.time()
         dct = mp.get_plan(
             required_dct,
             owned_dct,
@@ -53,5 +49,32 @@ async def plan(request):
     return response.json(dct)
 
 
+async def update_coro():
+    while True:
+        # Sleep an hour before checking for updates
+        await asyncio.sleep(60 * 60)
+        mp.update()
+
+
+def main():
+    serv_coro = app.create_server(host="localhost", port=8000, return_asyncio_server=True)
+    loop = asyncio.get_event_loop()
+    signal(SIGINT, lambda s, f: loop.stop())
+    serv_task = asyncio.ensure_future(serv_coro, loop=loop)
+    loop.run_until_complete(serv_task)
+    asyncio.Task(update_coro(), loop=loop)  # type: ignore
+    try:
+        loop.run_forever()
+    except:
+        loop.stop()
+
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000)
+    try:
+        import uvloop
+
+        asyncio.set_event_loop(uvloop.new_event_loop())
+        print("Using uvloop")
+    except:
+        print("Using asyncio loop")
+    main()
