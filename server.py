93d62b3cf7f5203eae59fe197e1e3939d55d37ac
kyoukaya <kyoukaya@gmail.com>
2020-02-11 22:38:37 +0800

    request validation

--- server.py
+++ server.py
@@ -4,44 +4,49 @@ from signal import SIGINT, signal
 from sanic import Sanic, response
 
 from MaterialPlanning import MaterialPlanning
+from marshmallow import Schema, fields, validate
+from marshmallow.exceptions import ValidationError
 
 app = Sanic(name="ArkPlanner")
 mp = MaterialPlanning()
-mp.update()
+# mp.update()
+
+
+class PlanSchema(Schema):
+    region = fields.Str(missing="en", validate=validate.OneOf(["en", "cn", "jp", "kr"]))
+    extra_outc = fields.Bool(missing=False)
+    exp_demand = fields.Bool(missing=False)
+    gold_demand = fields.Bool(missing=True)
+    required = fields.Dict(
+        keys=fields.String(),
+        values=fields.Integer(),
+        required=True,
+        validate=validate.Length(min=1),
+    )
+    owned = fields.Dict(keys=fields.Integer(), values=fields.Integer(), missing=None)
+
+
+schema = PlanSchema()
 
 
 @app.route("/plan", methods=["POST"])
 async def plan(request):
     try:
-        input_data = request.json
-        owned_dct = input_data["owned"]
-        required_dct = input_data["required"]
-    except:
-        return response.json({"error": True, "reason": "Uninterpretable input"})
+        request = schema.load(request.json)
+    except ValidationError as e:
+        return response.json({"error": {"request_validation_error": e.messages}})
 
-    try:
-        extra_outc = request.json["extra_outc"]
-    except:
-        extra_outc = False
-
-    try:
-        exp_demand = request.json["exp_demand"]
-    except:
-        exp_demand = True
-
-    try:
-        gold_demand = request.json["gold_demand"]
-    except:
-        gold_demand = True
+    if request["owned"] is None:
+        request["owned"] = {}
 
     try:
         dct = mp.get_plan(
-            required_dct,
-            owned_dct,
+            request["required"],
+            request["owned"],
             False,
-            outcome=extra_outc,
-            exp_demand=exp_demand,
-            gold_demand=gold_demand,
+            outcome=request["extra_outc"],
+            exp_demand=request["exp_demand"],
+            gold_demand=request["gold_demand"],
         )
     except ValueError as e:
         return response.json({"error": True, "reason": str(e)})
@@ -56,8 +61,17 @@ async def update_coro():
         mp.update()
 
 
-def main():
-    serv_coro = app.create_server(host="localhost", port=8000, return_asyncio_server=True)
+if __name__ == "__main__":
+    try:
+        import uvloop
+
+        asyncio.set_event_loop(uvloop.new_event_loop())
+        print("Using uvloop")
+    except:
+        print("Using asyncio loop")
+    serv_coro = app.create_server(
+        host="localhost", port=8000, return_asyncio_server=True
+    )
     loop = asyncio.get_event_loop()
     signal(SIGINT, lambda s, f: loop.stop())
     serv_task = asyncio.ensure_future(serv_coro, loop=loop)
@@ -67,14 +81,3 @@ def main():
         loop.run_forever()
     except:
         loop.stop()
-
-
-if __name__ == "__main__":
-    try:
-        import uvloop
-
-        asyncio.set_event_loop(uvloop.new_event_loop())
-        print("Using uvloop")
-    except:
-        print("Using asyncio loop")
-    main()
